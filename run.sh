3bbad936684da14af4ed6b1844503debc3676b92
Eric Jiang <3175220042ericjiang@gmail.com>
2025-09-20 12:41:23 +0800

    update run.sh in linux system and transfer command language

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 CLASS=$1
-LIB="`pwd`/libs/algs4.jar"
+shift   # 把第一个参数(类名)去掉，后面剩下的全是 main 的参数
+LIB="$(pwd)/libs/algs4.jar"
 SRC="src"
+OUT="out"
 
-CP=".:$LIB:$SRC"
+CP=".:$LIB:$OUT"
 
-# 编译
-javac -cp "$CP" "$SRC/$(echo $CLASS | tr '.' '/').java"
+# 编译到 out 目录，保持干净
+javac -d "$OUT" -cp "$LIB:$SRC" "$SRC/$(echo $CLASS | tr '.' '/').java"
 
-# 运行
-java -cp "$CP" $CLASS
+# 运行，支持 < 输入重定向
+java -cp "$CP" "$CLASS" "$@"

586bdfc8afc395b54aa83e491f6ad06ca0317ab6
Eric Jiang <202411079735@mail.bnu.edu.cn>
2025-09-21 10:01:41 +0800

    add average

--- run.ps1
+++ run.ps1
@@ -1,14 +1,24 @@
 param(
-    [string]$class
+    [string]$CLASS,          # 类名，例如 ch0.BinarySearch
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$Args          # 其余参数传递给 main
 )
 
-$lib = "D:\code\datastructure\libs\algs4.jar"
-$src = "src"
+$LIB  = "$(Get-Location)\libs\algs4.jar"
+$SRC  = "src"
+$OUT  = "out"
+$CP   = ".;$LIB;$OUT"
 
-$cp = ".;$lib;$src"
+# 确保 out 目录存在
+if (-not (Test-Path $OUT)) {
+    New-Item -ItemType Directory -Path $OUT | Out-Null
+}
+
+# 类名转路径，例如 ch0.BinarySearch → ch0\BinarySearch.java
+$Path = $CLASS -replace '\.', '\'
 
 # 编译
-javac -cp $cp "$src\$($class -replace '\.', '\\').java"
+& javac -d $OUT -cp "$LIB;$SRC" "$SRC\$Path.java"
 
 # 运行
-java -cp $cp $class
+& java -cp "$CP" $CLASS @Args
---
+++ src/ch0/Average.java
@@ -0,0 +1,17 @@
+package ch0;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Average {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int count = 0;
+        while(!StdIn.isEmpty())
+        {
+            sum += StdIn.readDouble();
+            count++;
+        }
+        StdOut.println(sum/count);
+    }
+}
